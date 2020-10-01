fb2ddfa90cb24cc335d4fc7c1de6bbb385344bbe
FynnKaeser <fynn.kaeser@icloud.com>
2020-10-01 17:13:21 +0200

    Part 1-3 of ASP.NET Core MVC app tutorial Part 4 up to and including Test the app

---
+++ src/biz.dfch.CS.Examples.SampleAspNetCoreWebApp/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using biz.dfch.CS.Examples.SampleAspNetCoreWebApp.Models;
+
+namespace biz.dfch.CS.Examples.SampleAspNetCoreWebApp.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- src/biz.dfch.CS.Examples.SampleAspNetCoreWebApp/Startup.cs
+++ src/biz.dfch.CS.Examples.SampleAspNetCoreWebApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using biz.dfch.CS.Examples.SampleAspNetCoreWebApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace biz.dfch.CS.Examples.SampleAspNetCoreWebApp
 {
@@ -24,6 +26,9 @@ namespace biz.dfch.CS.Examples.SampleAspNetCoreWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
