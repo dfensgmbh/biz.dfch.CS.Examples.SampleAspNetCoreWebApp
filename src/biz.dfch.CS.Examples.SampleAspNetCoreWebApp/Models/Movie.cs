857c46757b5bd6287ce63b333cf9b772b5404520
FynnKaeser <fynn.kaeser@icloud.com>
2020-10-02 16:41:32 +0200

    Part 4 - 6 Create a web app with ASP.NET Core MVC

--- src/biz.dfch.CS.Examples.SampleAspNetCoreWebApp/Models/Movie.cs
+++ src/biz.dfch.CS.Examples.SampleAspNetCoreWebApp/Models/Movie.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace biz.dfch.CS.Examples.SampleAspNetCoreWebApp.Models
 {
@@ -9,9 +9,12 @@ namespace biz.dfch.CS.Examples.SampleAspNetCoreWebApp.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
